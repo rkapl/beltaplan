c15c18b02850cdd18a90186d6e2afe597a1ecd88
Roman Kapl <code@rkapl.cz>
2019-03-07 02:34:08 +0100

    Show errors when loading game data.

--- src/app/data-loader.ts
+++ src/app/data-loader.ts
@@ -2,12 +2,18 @@ namespace App{
     export function loadData(version: string, cb: (data: GameData.Data) => void){
         var request = new XMLHttpRequest();
         request.addEventListener("load", function () {
+            if (request.status != 200) {
+                alert("Can not load factorio data");
+            }
             var data = <GameData.Data> JSON.parse(request.responseText)
             data.prefix = 'data/' + version + '/';
             data.version = version;
             
             request = new XMLHttpRequest();
             request.addEventListener("load", function() {
+                if (request.status != 200) {
+                    alert("Can not load factorio data");
+                }
                 data.settings = <GameData.Settings> JSON.parse(request.responseText);
                 cb(data);
             });
