5a12f471ea9f3a25747bcbc261d72f6baef40bec
Roman Kapl <code@rkapl.cz>
2019-03-07 02:14:46 +0100

    Remove JS commited by mistake.

--- src/libstorage/api.js
+++
@@ -1,9 +0,0 @@
-var LibStorage;
-(function (LibStorage) {
-    "connect" | "get" | "set" | "rename" | "delete";
-    provider: Provider;
-    location: Location;
-    document: Document;
-    data: Blob;
-})(LibStorage || (LibStorage = {}));
-//# sourceMappingURL=api.js.map
